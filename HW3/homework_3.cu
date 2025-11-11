8094069958e9e40b681fb70f851b8b105bf8efd9
Frederik Hüttemann <frederik.huettemann@gmail.com>
2025-11-11 10:43:40 +0100

    updated

--- HW3/homework_3.cu
+++ HW3/homework_3.cu
@@ -91,9 +91,27 @@ __global__ void gpu_ilp1(float *vec, float a, float b, float c, size_t ilp_num)
         }
     
     // "store in global memory"
-    vec[1 * (blockIdx.x * blockDim.x + threadIdx.x) + 0] = a;
-    
+    vec[1 * (blockIdx.x * blockDim.x + threadIdx.x) + 0] = a;   
 }
+__global__ void gpu_ilp4(float *vec, float a, float b, float c, size_t ilp_num) 
+{
+    float a0, a1, a2, a3;
+#pragma unroll 16
+        for (int i = 0; i < NUM_ITERATIONS; i++) 
+        {
+            a0 = a * b + c;
+            a1 = a * b + c;
+            a2 = a * b + c;
+            a3 = a * b + c;
+        }
+    
+    // "store in global memory"
+    vec[1 * (blockIdx.x * blockDim.x + threadIdx.x) + 0] = a0;
+    vec[1 * (blockIdx.x * blockDim.x + threadIdx.x) + 1] = a1;
+    vec[1 * (blockIdx.x * blockDim.x + threadIdx.x) + 2] = a2;
+    vec[1 * (blockIdx.x * blockDim.x + threadIdx.x) + 3] = a3;
+}
+
 
 bool checkResult(float *hostRef, float *gpuRef, const int N) {
     double eps = 1.0E-8;
@@ -112,18 +130,22 @@ double cpuSecond()
     return (double)tp.tv_sec + (double)tp.tv_usec * 1.e-6;
 }
 
-void run_ilp1(double* cpu_time, double* gpu_time)
+void run_ilp(double* cpu_time, double* gpu_time, bool global_mem = false, bool ilp4 = false)
 {
     float a = 1.0f;
     float b = 2.0f;
     float c = 3.0f;
 
     double t_start, t_end, flops;
+    size_t unrolling;
+    dim3 blockDim(16, 1);
+    if (ilp4) {
+        unrolling = 4;
+    } else {
+        unrolling = 1;
+    }
 
-    dim3 blockDim(256, 1);
-
-    const size_t unrolling = 1;
-    const size_t max_blocks = 32;
+    const size_t max_blocks = 256;
     const size_t vector_size = blockDim.x * blockDim.y * max_blocks * unrolling;
 
     const size_t vector_size_bytes = vector_size * sizeof(float);
@@ -145,17 +167,34 @@ void run_ilp1(double* cpu_time, double* gpu_time)
 
 
         // cpu computation
+        
         t_start = cpuSecond();
-        cpu_ilp1(cpu_vec, a, b, c, n);
+        if (!global_mem && !ilp4) {
+            cpu_ilp1(cpu_vec, a, b, c, n);
+        } else if (!global_mem && ilp4) {
+            cpu_ilp4(cpu_vec, a, b, c, n);
+        } else if (global_mem && ilp4) {
+            cpu_global_ilp4(cpu_vec, n);
+        } else {
+            cpu_global_ilp1(cpu_vec, n);
+        }
         t_end = cpuSecond();
         *cpu_time = t_end - t_start;
         flops =( 1e-6 * total_op) / (*cpu_time);
-        printf("cpu/ilp1: %zu MFLOP/s\n", (size_t)flops);
+        printf("cpu/%s/%s: %zu MFLOP/s\n", global_mem ? "global" : "local", ilp4 ? "ilp4" : "ilp1", (size_t)flops);
 
 
         // gpu computation
         t_start = cpuSecond();
-        gpu_ilp1<<<gridDim, blockDim>>>(gpu_vec, a, b, c, n);
+        if (!global_mem && !ilp4) {
+            gpu_ilp1<<<gridDim, blockDim>>>(gpu_vec, a, b, c, n);
+        } else if (!global_mem && ilp4) {
+            gpu_ilp4<<<gridDim, blockDim>>>(gpu_vec, a, b, c, n);
+        } else if (global_mem && ilp4) {
+            // gpu_global_ilp4<<<gridDim, blockDim>>>(gpu_vec, n);
+        } else {
+            // gpu_global_ilp1<<<gridDim, blockDim>>>(gpu_vec, n);
+        }
         cudaGetLastError();
         cudaDeviceSynchronize();
         t_end = cpuSecond();
@@ -163,8 +202,6 @@ void run_ilp1(double* cpu_time, double* gpu_time)
         flops = (1e-6 * total_op) / (*gpu_time);
         printf("gpu/ilp1/%zu: %zu MFLOP/s\n", n, (size_t)flops);
     }
-
-   
 
 
 
@@ -182,11 +219,8 @@ void run_ilp1(double* cpu_time, double* gpu_time)
 int main() {
     // compare gpu_ilp1 and cpu_ilp1
     double cpu_time, gpu_time;
-    run_ilp1(&cpu_time, &gpu_time);
+    run_ilp(&cpu_time, &gpu_time);
     int num_op = 1 * NUM_ITERATIONS * NUM_REPETITIONS;
-    printf("ILP1 - GFLOPS CPU: %f\n", (double)num_op / cpu_time / 1e9);
-    printf("ILP1 - GFLOPS GPU: %f\n", (double)num_op / gpu_time / 1e9);
-    printf("ILP1 - CPU time: %f seconds\n", cpu_time);
-    printf("ILP1 - GPU time: %f seconds\n", gpu_time);
+    
 
 }  
